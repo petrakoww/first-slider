d9b7153d66abf9bf07557cd8fa0b2c2722026919
petrakoww <vasil4y@abv.bg>
2021-10-15 16:50:46 +0300

    Added dynamic features

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const carouselSlide = document.querySelector(".carousel-slide");
+const slider = document.querySelector(".carousel-slide");
 const carouselImages = document.querySelectorAll(".carousel-slide img");
 
 
@@ -9,50 +9,45 @@ let counter = 0;
 const size = carouselImages[0].clientWidth;
 
 
-firstPic = document.getElementById("firstPic");
-secondPic = document.getElementById("secondPic");
-thirdPic = document.getElementById("thirdPic");
+//dynamic circle
+const sliderContainer = document.querySelector('.carousel-container');
+const numberOfItems = slider.children.length;
 
+const circleDiv = document.createElement("div");
+circleDiv.style.textAlign = "center";
+sliderContainer.appendChild(circleDiv);
+let i;
+let test;
+for (i = 0; i < numberOfItems; i++) { 
+    test = document.createElement("button");
+    test.className = "dot";
+    test.id = i;
+    circleDiv.appendChild(test);
+}
 
+circleDiv.addEventListener("click", (e)=>{
+    const {id} = e.target;
 
+    if (id) {
+        slider.style.transform = "translateX(" + (-size * + id) + "px)";
+    }
+    counter = id;
+})
+
+//dynamic arrows
 nextBtn.addEventListener("click", () =>{
-    
     counter++;
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-    if (counter >= 3) {
-        carouselSlide.style.transform = "translateX(" + (0) + "px)";
+    if (counter >= i) {
         counter = 0;
-    };
+    }
+    slider.style.transform = "translateX(" + (-size * counter) + "px)";
 });
+
 
 prevBtn.addEventListener("click", () =>{
-    
     counter--;
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-    if (counter === -1) {
-        carouselSlide.style.transform = "translateX(" + (-1000) + "px)";
-        counter = 2;
-    };
-});
-
-
-
-firstPic.addEventListener("click", () =>{
-    counter = 0;
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-    if (counter === -1) {
-        carouselSlide.style.transform = "translateX(" + (-1000) + "px)";
-        counter = 0;
-    };
-});
-
-secondPic.addEventListener("click", () =>{
-    counter = 1;
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
-});
-
-thirdPic.addEventListener("click", () =>{
-    
-    counter = 2;
-    carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+    if (counter < 0) {
+        counter = i - 1;
+    }
+    slider.style.transform = "translateX(" + (-size * counter) + "px)";
 });
